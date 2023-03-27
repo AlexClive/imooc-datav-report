d8e38276e33e738e9ce158bb343107b53610e75e
zhangxuerong <1027092674@qq.com>
2023-03-27 17:12:05 +0800

    add: vue-eCharts

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import * as echarts from 'echarts'
+// import VueEChate from 'vue-echarts'
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = echarts
-
+// Vue.component('v-chart',VueEchats)
 new Vue({
   router,
   render: h => h(App)
